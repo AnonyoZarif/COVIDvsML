9e301b573b67f285c303f1f4fbc8048931ef30e6
unknown <anonyo.akand@yahoo.com>
2022-07-07 08:02:03 +0600

    updated django settings

--- CO_BEAT_Webapp/settings.py
+++ CO_BEAT_Webapp/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&m$yyjrz-)ixehve7rxe7!vi_dd!+i)=hu%6x7b074csf0-^#h'
+SECRET_KEY = "YOUR-KEY_MINE-REMOVED-FOR-SECURITY-PURPOSES"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
